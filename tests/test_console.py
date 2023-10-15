4d455e120a3f23d9a915fb152309fd494f0a1377
masilelazile <masilelazile@gmail.com>
2023-10-15 22:46:22 +0000

    console

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,54 +1,81 @@
-#!/usr/bin/python3
-"""Defines unittests for console.py."""
-
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 from io import StringIO
-from console import HBNBCommand 
+from console import HBNBCommand
+from models import storage
+from models.base_model import BaseModel
 
-class TestHBNBCommand(unittest.TestCase):
+class TestHBNBConsole(unittest.TestCase):
     def setUp(self):
         self.console = HBNBCommand()
+        self.mock_stdout = StringIO()
+        self.mock_stdout_patch = patch('sys.stdout', new=self.mock_stdout)
+        self.mock_stdout_patch.start()
 
     def tearDown(self):
-        pass  
+        self.mock_stdout_patch.stop()
+        del self.console
+        storage.reset()
 
-    def test_create(self):
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+    def test_create_command(self):
+        with patch('sys.stdout', new=StringIO()) as f:
             self.console.onecmd("create BaseModel")
-            output = mock_stdout.getvalue().strip()
-        self.assertTrue(output.startswith("****"))
+            output = f.getvalue().strip()
+            self.assertTrue(output.isalnum())
 
+    def test_show_command(self):
+        obj = BaseModel()
+        obj_id = obj.id
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd("show BaseModel {}".format(obj_id))
+            output = f.getvalue().strip()
+            self.assertTrue(str(obj) in output)
 
-    def test_show(self):
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-            self.console.onecmd("show BaseModel")
-            output = mock_stdout.getvalue().strip()
-        self.assertTrue(output.startswith("**"))
+    def test_destroy_command(self):
+        obj = BaseModel()
+        obj_id = obj.id
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd("destroy BaseModel {}".format(obj_id))
+            output = f.getvalue().strip()
+            self.assertEqual(output, '')
+            self.assertIsNone(storage.get("BaseModel", obj_id))
 
-    def test_destroy(self):
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-            self.console.onecmd("destroy BaseModel")
-            output = mock_stdout.getvalue().strip()
-        self.assertTrue(output.startswith("**"))
+    def test_all_command(self):
+        obj1 = BaseModel()
+        obj2 = BaseModel()
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd("all BaseModel")
+            output = f.getvalue().strip()
+            self.assertTrue(str(obj1) in output)
+            self.assertTrue(str(obj2) in output)
 
-    def test_all(self):
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-            self.console.onecmd("all")
-            output = mock_stdout.getvalue().strip()
-        self.assertTrue(output.startswith("****"))
-
-    def test_count(self):
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+    def test_count_command(self):
+        obj1 = BaseModel()
+        obj2 = BaseModel()
+        with patch('sys.stdout', new=StringIO()) as f:
             self.console.onecmd("count BaseModel")
-            output = mock_stdout.getvalue().strip()
-        self.assertTrue(output.isdigit())
+            output = f.getvalue().strip()
+            self.assertEqual(output, '2')
 
-    def test_update(self):
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-            self.console.onecmd("update BaseModel")
-            output = mock_stdout.getvalue().strip()
-        self.assertTrue(output.startswith("**"))
+    def test_update_command(self):
+        obj = BaseModel()
+        obj_id = obj.id
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd("update BaseModel {} name 'NewName'".format(obj_id))
+            output = f.getvalue().strip()
+            self.assertEqual(output, '')
+            self.assertEqual(obj.name, 'NewName')
+
+    def test_update_command_with_dict(self):
+        obj = BaseModel()
+        obj_id = obj.id
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd("update BaseModel {} {'name': 'NewName', 'age': 25}".format(obj_id))
+            output = f.getvalue().strip()
+            self.assertEqual(output, '')
+            self.assertEqual(obj.name, 'NewName')
+            self.assertEqual(obj.age, 25)
 
 if __name__ == '__main__':
     unittest.main()
+
